788762054f117f5881758bf6ebbef10f27436629
Deep Darji <darjideep74@gmail.com>
2024-10-11 12:22:00 +0530

    Dropdown Setup

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  late double _deviceHeight, _deviceWidth;
+
   HomePage(Key? key) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: _pageTitle(),
+      body: SafeArea(
+          child: Container(
+        height: _deviceHeight,
+        width: _deviceWidth,
+        padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _pageTitle(),
+            _destinationDropDown(),
+          ],
+        ),
+      )),
     );
   }
 
+//Title
   Widget _pageTitle() {
     return const Text(
       "#GoMoon",
@@ -21,12 +40,60 @@ class HomePage extends StatelessWidget {
     );
   }
 
+//Image
   Widget _astroImageWidget() {
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(
           fit: BoxFit.fill,
           image: AssetImage("assets/images/astro_moon.png"),
+        ),
+      ),
+    );
+  }
+
+//Dropdown1
+  Widget _destinationDropDown() {
+    List<String> _items = [
+      'ISRO',
+      'NASA',
+      'SPACE X',
+    ];
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: _deviceWidth * 0.05),
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(
+          53,
+          53,
+          53,
+          1.0,
+        ),
+        borderRadius: BorderRadius.circular(
+          10,
+        ),
+      ),
+      child: DropdownButton(
+        value: _items.first,
+        onChanged: (_) {},
+        items: _items.map(
+          (e) {
+            return DropdownMenuItem(
+              child: Text(e),
+              value: e,
+            );
+          },
+        ).toList(),
+        underline: Container(),
+        dropdownColor: const Color.fromRGBO(
+          53,
+          53,
+          53,
+          1.0,
+        ),
+        style: const TextStyle(
+          color: Colors.white,
         ),
       ),
     );
